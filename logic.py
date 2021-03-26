8a8c1ad8af632bcd50a276afe71cb0a25c278dbe
Grzegorz Maciaszek <grzesiek@devdo.eu>
2021-03-26 13:48:36 +0100

    horde logic implemented.

--- logic.py
+++ logic.py
@@ -30,6 +30,20 @@ class GameState:
         shuffle(deck)
         self.city_deck = deck
 
+    def shuffle_city_graveyard(self):
+        new_city = self.city_graveyard
+        shuffle(new_city)
+        self.city_graveyard = []
+        self.city_deck = self.city_deck + new_city
+
+    def get_city_card(self):
+        if len(self.city_deck) < 1:
+            self.shuffle_city_graveyard()
+        if len(self.city_deck) < 1:
+            return None
+        card = self.city_deck.pop(0)
+        return card
+
     def prepare_supply_deck(self):
         defense = ['alarm'] * 2 + ['mine field'] * 2 + ['barricades'] * 4
         summons = ['radio'] * 4 + ['megaphone'] * 4 + ['flare gun'] * 3
@@ -41,21 +55,37 @@ class GameState:
         self.supply_deck = supply_deck
 
     def zombie_show_up(self):
-        if len(self.city_deck) < 1:
+        card = self.get_city_card()
+        if card is None:
             return
-        card = self.city_deck[0]
 
-        if card[0] == 'survivor' and card[1] != 'fast zombie':
+        if card[0] == 'survivor' and card[1] not in ['fast zombie', 'horde']:
             card[0], card[1] = card[1], card[0]
+            self.city_deck = [card] + self.city_deck
             return
         elif card[1] == 'fast zombie':
             card[0], card[1] = card[1], card[0]
         elif card[1] == 'horde':
             self.event_horde()
+            self.city_graveyard.append(card)
+            return
 
         self.active_player.zombies.append(card[0])
-        self.city_deck.remove(card)
 
-    def event_horde(self):
-        self.city_graveyard.append(self.city_deck[0])
-        self.city_deck.remove(self.city_deck[0])
+    def event_horde(self, second=False):
+        index = self.players.index(self.active_player)
+        helper_table = self.players + self.players
+        helper_table = helper_table[index + 1: index + len(self.players) + 1]
+
+        for player in helper_table:
+            card = self.get_city_card()
+            if card is None or (second and player == self.active_player):
+                continue
+            if card[1] == 'horde':
+                self.event_horde(True)
+                self.city_graveyard.append(card)
+                card = self.get_city_card()
+                if card is not None:
+                    player.zombies.append(card[1])
+            else:
+                player.zombies.append(card[1])
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -58,6 +58,14 @@ def test_zombie_show_up():
     assert len(gs.city_deck) == 0
 
 
+def test_zombie_show_up_empty_city():
+    gs = logic.GameState()
+    gs.active_player = logic.PlayerShelter()
+    gs.city_deck = []
+    gs.zombie_show_up()
+    assert len(gs.active_player.zombies) == 0
+
+
 def test_fast_zombie_show_up():
     gs = logic.GameState()
     gs.active_player = logic.PlayerShelter()
@@ -71,11 +79,79 @@ def test_fast_zombie_show_up():
 def test_horde_show_up():
     gs = logic.GameState()
     gs.players = [logic.PlayerShelter(), logic.PlayerShelter(), logic.PlayerShelter()]
-    gs.active_player = gs.players[0]
+    gs.active_player = gs.players[1]
     gs.city_deck = [['survivor', 'horde'], ['survivor', 'zombie'], ['survivor', 'zombie'], ['survivor', 'zombie'],
                     ['survivor', 'zombie'], ['survivor', 'zombie'], ['survivor', 'zombie'], ['survivor', 'zombie']]
     gs.zombie_show_up()
+    assert len(gs.city_deck) == 4
     assert len(gs.players[0].zombies) == 1
     assert len(gs.players[1].zombies) == 1
+    assert len(gs.players[2].zombies) == 1
+    assert len(gs.active_player.zombies) == 1
+
+
+def test_horde_show_up_two_zombies_left_in_city_no_graveyard():
+    gs = logic.GameState()
+    gs.players = [logic.PlayerShelter(), logic.PlayerShelter(), logic.PlayerShelter()]
+    gs.active_player = gs.players[1]
+    gs.city_deck = [['survivor', 'horde'], ['survivor', 'zombie'], ['survivor', 'zombie']]
+    gs.zombie_show_up()
+    assert len(gs.city_deck) == 0
+    assert len(gs.players[0].zombies) == 1
+    assert len(gs.players[1].zombies) == 0
+    assert len(gs.players[2].zombies) == 1
+    assert len(gs.active_player.zombies) == 0
+
+    gs = logic.GameState()
+    gs.players = [logic.PlayerShelter(), logic.PlayerShelter(), logic.PlayerShelter()]
+    gs.active_player = gs.players[2]
+    gs.city_deck = [['survivor', 'horde'], ['survivor', 'zombie'], ['survivor', 'zombie']]
+    gs.zombie_show_up()
+    assert len(gs.city_deck) == 0
+    assert len(gs.players[0].zombies) == 1
+    assert len(gs.players[1].zombies) == 1
+    assert len(gs.players[2].zombies) == 0
+    assert len(gs.active_player.zombies) == 0
+
+
+def test_horde_show_up_two_zombies_left_in_city_more_on_graveyard():
+    gs = logic.GameState()
+    gs.players = [logic.PlayerShelter(), logic.PlayerShelter(), logic.PlayerShelter()]
+    gs.active_player = gs.players[1]
+    gs.city_deck = [['survivor', 'horde'], ['survivor', 'zombie'], ['survivor', 'zombie']]
+    gs.city_graveyard = [['survivor', 'zombie'], ['survivor', 'zombie'], ['survivor', 'zombie']]
+    gs.zombie_show_up()
+    assert len(gs.city_deck) == 2
+    assert len(gs.city_graveyard) == 1
+    assert len(gs.players[0].zombies) == 1
+    assert len(gs.players[1].zombies) == 1
+    assert len(gs.players[2].zombies) == 1
+    assert len(gs.active_player.zombies) == 1
+
+
+def test_horde_show_up_second_time():
+    gs = logic.GameState()
+    gs.players = [logic.PlayerShelter(), logic.PlayerShelter(), logic.PlayerShelter()]
+    gs.active_player = gs.players[1]
+    gs.city_deck = [['survivor', 'horde'], ['survivor', 'zombie'], ['survivor', 'horde'], ['survivor', 'zombie'],
+                    ['survivor', 'zombie'], ['survivor', 'zombie'], ['survivor', 'zombie'], ['survivor', 'zombie']]
+    gs.zombie_show_up()
+    assert len(gs.city_deck) == 0
+    assert len(gs.city_graveyard) == 2
+    assert len(gs.players[0].zombies) == 2
+    assert len(gs.players[1].zombies) == 1
+    assert len(gs.players[2].zombies) == 2
+    assert len(gs.active_player.zombies) == 1
+
+    gs = logic.GameState()
+    gs.players = [logic.PlayerShelter(), logic.PlayerShelter(), logic.PlayerShelter()]
+    gs.active_player = gs.players[2]
+    gs.city_deck = [['survivor', 'horde'], ['survivor', 'horde'], ['survivor', 'zombie'], ['survivor', 'zombie'],
+                    ['survivor', 'zombie'], ['survivor', 'zombie'], ['survivor', 'zombie'], ['survivor', 'zombie']]
+    gs.zombie_show_up()
+    assert len(gs.city_deck) == 0
+    assert len(gs.city_graveyard) == 2
+    assert len(gs.players[0].zombies) == 2
+    assert len(gs.players[1].zombies) == 2
     assert len(gs.players[2].zombies) == 1
     assert len(gs.active_player.zombies) == 1
