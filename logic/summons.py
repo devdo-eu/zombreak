59536408b5ceaa9e8851fded9c8a98e9d9e1b58a
Grzegorz Maciaszek <grzesiek@devdo.eu>
2021-04-07 07:16:15 +0200

    Further work on source documentation.

--- logic/summons.py
+++ logic/summons.py
@@ -3,7 +3,12 @@ from enums.zombie import ZombieType
 from enums.supply import Supply
 
 
-async def play_summon(game_state, summon):
+async def play_summon(game_state, summon: Supply) -> None:
+    """
+    Function used to control of play of any summon card.
+    :param game_state: GameState object with all game data inside
+    :param summon: Supply enum with played card
+    """
     shelter = game_state.active_player
     arrives = 1
     if summon == Supply.FLARE_GUN:
@@ -25,13 +30,25 @@ async def play_summon(game_state, summon):
     put_supplies_on_graveyard(game_state, summon)
 
 
-async def play_radio(game_state):
+async def play_radio(game_state) -> None:
+    """
+    Function used to control of play of RADIO card.
+    :param game_state: GameState object with all game data inside
+    """
     await play_summon(game_state, Supply.RADIO)
 
 
-async def play_megaphone(game_state):
+async def play_megaphone(game_state) -> None:
+    """
+    Function used to control of play of MEGAPHONE card.
+    :param game_state: GameState object with all game data inside
+    """
     await play_summon(game_state, Supply.MEGAPHONE)
 
 
-async def play_flare_gun(game_state):
+async def play_flare_gun(game_state) -> None:
+    """
+    Function used to control of play of FLARE GUN card.
+    :param game_state: GameState object with all game data inside
+    """
     await play_summon(game_state, Supply.FLARE_GUN)
